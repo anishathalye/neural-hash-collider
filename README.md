108b526a86c1ac517e44191d47f5722b8f99b7ce
Anish Athalye <me@anishathalye.com>
2021-08-22 16:47:58 -0400

    Switch to fancier attack for nicer-looking results

--- README.md
+++ README.md
@@ -8,10 +8,8 @@ we can find an adversarial image that has the same hash as the
 [picture](https://user-images.githubusercontent.com/1328/129860794-e7eb0132-d929-4c9d-b92e-4e4faba9e849.png)
 of the dog in [this post][hash collisions]:
 
-```console
-$ python collide.py --image cat.jpg --target 59a34eabe31910abfb06f308
-...
-# took about 2.5 minutes to run on an i7-5930K
+```bash
+python collide.py --image cat.jpg --target 59a34eabe31910abfb06f308
 ```
 
 ![Cat image with NeuralHash 59a34eabe31910abfb06f308](https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/cat-adv.png) ![Dog image with NeuralHash 59a34eabe31910abfb06f308](https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/dog.png)
@@ -49,11 +47,63 @@ hash: this loss is basically just the NeuralHash algorithm as described above,
 but with the final "hard" thresholding step tweaked so that it is "soft" (in
 particular, differentiable). Exactly how this is done (choices of activation
 functions, parameters, etc.) can affect convergence, so it can require some
-experimentation. Refer to `collide.py` to see what the implementation currently
-does.
+experimentation. After choosing the loss function, we can follow the standard
+method to find adversarial examples for neural networks: gradient descent.
 
-After choosing the loss function, we can follow the standard method to find
-adversarial examples for neural networks: we perform gradient descent.
+### Details
+
+The implementation currently does an alternating projections style attack to
+find an adversarial example that has the intended hash and also looks similar
+to the original. See `collide.py` for the full details. The implementation uses
+two different loss functions: one measures the distance to the target hash, and
+the other measures the quality of the perturbation (l2 norm + total variation).
+We first optimize for a collision, focusing only on matching the target hash.
+Once we find a projection, we alternate between minimizing the perturbation and
+ensuring that the hash value does not change. The attack has a number of
+parameters; run `python collide.py --help` or refer to the code for a full
+list. Tweaking these parameters can make a big difference in convergence time
+and the quality of the output.
+
+The implementation also supports a flag `--blur [sigma]` that blurs the
+perturbation on every step of the search. This can slow down or break
+convergence, but on some examples, it can be helpful for getting results that
+look more natural and less like glitch art.
+
+## Examples
+
+Reproducing the [Lena](https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/lena.png)/[Barbara](https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/barbara.png) result from [this post](https://github.com/AsuharietYgvar/AppleNeuralHash2ONNX/issues/1#issuecomment-903094036):
+
+<img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/lena.png"></img> <img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/lena-adv.png"></img> <img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/lena-adv-blur-1.0.png"></img> <img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/barbara.png"></img>
+
+The first image above is the original Lena image. The second was produced with `--target a426dae78cc63799d01adc32` to collide with Barbara. The third was produced with the additional argument `--blur 1.0`. The fourth is the original Barbara image. Checking their hashes:
+
+```console
+$ python nnhash.py lena.png
+32dac883f7b91bbf45a48296
+$ python nnhash.py lena-adv.png
+a426dae78cc63799d01adc32
+$ python nnhash.py lena-adv-blur-1.0.png
+a426dae78cc63799d01adc32
+$ python nnhash.py barbara.png
+a426dae78cc63799d01adc32
+```
+
+Reproducing the [Picard](https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/picard.png)/[Sidious](https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/sidious.png) result from [this post](https://github.com/anishathalye/neural-hash-collider/issues/4):
+
+<img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/picard.png"></img> <img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/picard-adv.png"></img> <img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/picard-adv-blur-0.5.png"></img> <img width="200" src="https://raw.githubusercontent.com/anishathalye/neural-hash-collider/assets/sidious.png"></img>
+
+The first image above is the original Picard image. The second was produced with `--target e34b3da852103c3c0828fbd1 --tv-weight 3e-4` to collide with Sidious. The third was produced with the additional argument `--blur 0.5`. The fourth is the original Sidious image. Checking their hashes:
+
+```console
+$ python nnhash.py picard.png
+73fae120ad3191075efd5580
+$ python nnhash.py picard-adv.png
+e34b2da852103c3c0828fbd1
+$ python nnhash.py picard-adv-blur-0.5.png
+e34b2da852103c3c0828fbd1
+$ python nnhash.py sidious.png
+e34b2da852103c3c0828fbd1
+```
 
 ## Prerequisites
 
@@ -77,16 +127,3 @@ The code in this repository is intended to be a demonstration, and perhaps a
 starting point for other exploration. Tweaking the implementation (choice of
 loss function, choice of parameters, etc.) might produce much better results
 than this code currently achieves.
-
-The code in this repository currently implements a simple loss function that
-just measures the distance to the target hash value. It happens to be the case
-that starting from a particular image produces a final image that looks
-somewhat similar; to better enforce this property, the loss function could be
-modified to add a penalty for making the image look different, e.g. l2 distance
-between the original image and the computed adversarial example (another
-standard technique), or we could use projected gradient descent to project onto
-an l-infinity ball centered at the original image as we optimize (yet another
-standard technique).
-
-The code in this repository does not currently use any fancy optimization
-algorithm, just vanilla gradient descent.
--- collide.py
+++ collide.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import tensorflow as tf
+from scipy.ndimage.filters import gaussian_filter
 import argparse
 import os
 
@@ -11,11 +12,16 @@ from util import *
 DEFAULT_MODEL_PATH = 'model.onnx'
 DEFAULT_SEED_PATH = 'neuralhash_128x96_seed1.dat'
 DEFAULT_TARGET_HASH = '59a34eabe31910abfb06f308'
-DEFAULT_ITERATIONS = 1000
+DEFAULT_ITERATIONS = 10000
+DEFAULT_SAVE_ITERATIONS = 0
 DEFAULT_LR = 2.0
+DEFAULT_COMBINED_THRESHOLD = 2
 DEFAULT_K = 10.0
 DEFAULT_CLIP_RANGE = 0.1
-DEFAULT_SAVE_ITERATIONS = 100
+DEFAULT_W_L2 = 2e-3
+DEFAULT_W_TV = 1e-4
+DEFAULT_W_HASH = 0.8
+DEFAULT_BLUR = 0
 
 
 def main():
@@ -29,7 +35,7 @@ def main():
 
     target = hash_from_hex(options.target)
 
-    x = load_image(options.image)
+    original = load_image(options.image)
     h = hash_from_hex(options.target)
 
     with model.graph.as_default():
@@ -37,36 +43,82 @@ def main():
             sess.run(tf.compat.v1.global_variables_initializer())
 
             proj = tf.reshape(tf.linalg.matmul(seed, tf.reshape(logits, (128, 1))), (96,))
+            # proj is in R^96; it's interpreted as a 96-bit hash by mapping
+            # entries < 0 to the bit '0', and entries >= 0 to the bit '1'
             normalized, _ = tf.linalg.normalize(proj)
             hash_output = tf.sigmoid(normalized * options.k)
+            # now, hash_output has entries in (0, 1); it's interpreted by
+            # mapping entries < 0.5 to the bit '0' and entries >= 0.5 to the
+            # bit '1'
 
+            # we clip hash_output to (clip_range, 1-clip_range); this seems to
+            # improve the search (we don't "waste" perturbation tweaking
+            # "strong" bits); the sigmoid already does this to some degree, but
+            # this seems to help
             hash_output = tf.clip_by_value(hash_output, options.clip_range, 1.0 - options.clip_range) - 0.5
             hash_output = hash_output * (0.5 / (0.5 - options.clip_range))
             hash_output = hash_output + 0.5
 
-            loss = tf.math.reduce_sum(tf.math.squared_difference(hash_output, h))
-            g, = tf.gradients(loss, image)
+            # hash loss: how far away we are from the target hash
+            hash_loss = tf.math.reduce_sum(tf.math.squared_difference(hash_output, h))
 
-            best = float('inf')
+            perturbation = image - original
+            # image loss: how big / noticeable is the perturbation?
+            img_loss = options.l2_weight * tf.nn.l2_loss(perturbation) + options.tv_weight * tf.image.total_variation(perturbation)[0]
+
+            # combined loss: try to minimize both at once
+            combined_loss = options.hash_weight * hash_loss + (1 - options.hash_weight) * img_loss
+
+            # gradients of all the losses
+            g_hash_loss, = tf.gradients(hash_loss, image)
+            g_img_loss, = tf.gradients(img_loss, image)
+            g_combined_loss, = tf.gradients(combined_loss, image)
+
+            # perform attack
+
+            x = original
+            best = (float('inf'), 0)  # (distance, image quality loss)
+            dist = float('inf')
 
             for i in range(options.iterations):
-                xq = quantize(x)
-                hash_output_v, loss_v, g_v = sess.run([hash_output, loss, g], feed_dict={image: xq})
+                # we do an alternating projections style attack here; if we
+                # haven't found a colliding image yet, only optimize for that;
+                # if we have a colliding image, then minimize the size of the
+                # perturbation; if we're close, then do both at once
+                if dist == 0:
+                    loss_name, loss, g = 'image', img_loss, g_img_loss
+                elif best[0] == 0 and dist <= options.combined_threshold:
+                    loss_name, loss, g = 'combined', combined_loss, g_combined_loss
+                else:
+                    loss_name, loss, g = 'hash', hash_loss, g_hash_loss
+
+                # compute loss values and gradient
+                xq = quantize(x)  # take derivatives wrt the quantized version of the image
+                hash_output_v, img_loss_v, loss_v, g_v = sess.run([hash_output, img_loss, loss, g], feed_dict={image: xq})
                 dist = np.sum((hash_output_v >= 0.5) != (h >= 0.5))
-                if dist < best or (i+1) % options.save_iterations == 0:
-                    save_image(x, os.path.join(options.save_directory, 'out_iter={:05d}_dist={:02d}.png'.format(i+1, dist)))
-                if dist < best:
-                    best = dist
+
+                # if it's better than any image found so far, save it
+                score = (dist, img_loss_v)
+                if score < best or (options.save_iterations > 0 and (i+1) % options.save_iterations == 0):
+                    save_image(x, os.path.join(options.save_directory, 'out_iter={:05d}_dist={:02d}_q={:.3f}.png'.format(i+1, dist, img_loss_v)))
+                if score < best:
+                    best = score
+
+                # gradient descent step
                 g_v_norm = g_v / np.linalg.norm(g_v)
                 x = x - options.learning_rate * g_v_norm
+                if options.blur > 0:
+                    x = blur_perturbation(original, x, options.blur)
                 x = x.clip(-1, 1)
-                print('iteration: {}/{}, best: {}, hash: {}, distance: {}, loss: {:.3f}'.format(
+                print('iteration: {}/{}, best: ({}, {:.3f}), hash: {}, distance: {}, loss: {:.3f} ({})'.format(
                     i+1,
                     options.iterations,
-                    best,
+                    best[0],
+                    best[1],
                     hash_to_hex(hash_output_v),
                     dist,
-                    loss_v
+                    loss_v,
+                    loss_name
                 ))
 
 
@@ -77,18 +129,33 @@ def quantize(x):
     return x
 
 
+def blur_perturbation(original, x, sigma):
+    perturbation = x - original
+    perturbation = gaussian_filter_by_channel(perturbation, sigma=sigma)
+    return original + perturbation
+
+
+def gaussian_filter_by_channel(x, sigma):
+    return np.stack([gaussian_filter(x[0,ch,:,:], sigma) for ch in range(x.shape[1])])[np.newaxis]
+
+
 def get_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image', help='path to starting image', required=True)
-    parser.add_argument('--model', help='path to model', default=DEFAULT_MODEL_PATH)
-    parser.add_argument('--seed', help='path to seed', default=DEFAULT_SEED_PATH)
-    parser.add_argument('--target', help='target hash', default=DEFAULT_TARGET_HASH)
+    parser.add_argument('--image', type=str, help='path to starting image', required=True)
+    parser.add_argument('--model', type=str, help='path to model', default=DEFAULT_MODEL_PATH)
+    parser.add_argument('--seed', type=str, help='path to seed', default=DEFAULT_SEED_PATH)
+    parser.add_argument('--target', type=str, help='target hash', default=DEFAULT_TARGET_HASH)
     parser.add_argument('--learning-rate', type=float, help='learning rate', default=DEFAULT_LR)
+    parser.add_argument('--combined-threshold', type=int, help='threshold to start using combined loss', default=DEFAULT_COMBINED_THRESHOLD)
     parser.add_argument('--k', type=float, help='k parameter', default=DEFAULT_K)
+    parser.add_argument('--l2-weight', type=float, help='perturbation l2 loss weight', default=DEFAULT_W_L2)
+    parser.add_argument('--tv-weight', type=float, help='perturbation total variation loss weight', default=DEFAULT_W_TV)
+    parser.add_argument('--hash-weight', type=float, help='relative weight (0.0 to 1.0) of hash in combined loss', default=DEFAULT_W_HASH)
     parser.add_argument('--clip-range', type=float, help='clip range parameter', default=DEFAULT_CLIP_RANGE)
     parser.add_argument('--iterations', type=int, help='max number of iterations', default=DEFAULT_ITERATIONS)
-    parser.add_argument('--save-directory', help='directory to save output images', default='.')
-    parser.add_argument('--save-iterations', type=int, help='save every _ iterations, regardless of improvement', default=DEFAULT_SAVE_ITERATIONS)
+    parser.add_argument('--save-directory', type=str, help='directory to save output images', default='.')
+    parser.add_argument('--save-iterations', type=int, help='save this frequently, regardless of improvement', default=DEFAULT_SAVE_ITERATIONS)
+    parser.add_argument('--blur', type=float, help='apply Gaussian blur with this sigma on every step', default=DEFAULT_BLUR)
     return parser.parse_args()
 
 
